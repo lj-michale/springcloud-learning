44763322afa661573141d95be906571086eb29a1
Tony <economic_luojie@outlook.com>
2023-10-11 11:02:59 +0800

    feat: JDK版本调整

---
+++ services/user-service/src/main/java/org/springcloud/turing/biz/service/impl/UserLoginServiceImpl.java
@@ -0,0 +1,252 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springcloud.turing.biz.service.impl;
+
+import cn.hutool.core.util.StrUtil;
+import com.alibaba.fastjson2.JSON;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springcloud.turing.biz.common.constant.RedisKeyConstant;
+import org.springcloud.turing.biz.common.enums.UserChainMarkEnum;
+import org.springcloud.turing.biz.common.enums.UserRegisterErrorCodeEnum;
+import org.springcloud.turing.biz.dao.entity.req.UserDeletionReqDTO;
+import org.springcloud.turing.biz.dao.entity.req.UserLoginReqDTO;
+import org.springcloud.turing.biz.dao.entity.req.UserRegisterReqDTO;
+import org.springcloud.turing.biz.dao.entity.resp.UserLoginRespDTO;
+import org.springcloud.turing.biz.dao.entity.resp.UserQueryRespDTO;
+import org.springcloud.turing.biz.dao.entity.resp.UserRegisterRespDTO;
+import org.springcloud.turing.biz.service.UserLoginService;
+import org.springcloud.turing.biz.service.UserService;
+import org.springcloud.turing.biz.toolkit.UserReuseUtil;
+import org.springcloud.turing.biz.dao.entity.UserDO;
+import org.springcloud.turing.biz.dao.entity.UserDeletionDO;
+import org.springcloud.turing.biz.dao.entity.UserMailDO;
+import org.springcloud.turing.biz.dao.entity.UserPhoneDO;
+import org.springcloud.turing.biz.dao.entity.UserReuseDO;
+import org.springcloud.turing.biz.dao.mapper.UserDeletionMapper;
+import org.springcloud.turing.biz.dao.mapper.UserMailMapper;
+import org.springcloud.turing.biz.dao.mapper.UserMapper;
+import org.springcloud.turing.biz.dao.mapper.UserPhoneMapper;
+import org.springcloud.turing.biz.dao.mapper.UserReuseMapper;
+import org.springcloud.turing.framework.starter.cache.DistributedCache;
+import org.springcloud.turing.framework.starter.common.toolkit.BeanUtil;
+import org.springcloud.turing.framework.starter.convention.exception.ClientException;
+import org.springcloud.turing.framework.starter.convention.exception.ServiceException;
+import org.springcloud.turing.framework.starter.designpattern.chain.AbstractChainContext;
+import org.springcloud.turing.framework.starter.user.core.UserContext;
+import org.springcloud.turing.framework.starter.user.core.UserInfoDTO;
+import org.springcloud.turing.framework.starter.user.toolkit.JWTUtil;
+import org.redisson.api.RBloomFilter;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Objects;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 用户登录接口实现
+ *
+ *
+ */
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class UserLoginServiceImpl implements UserLoginService {
+
+    private final UserService userService;
+    private final UserMapper userMapper;
+    private final UserReuseMapper userReuseMapper;
+    private final UserDeletionMapper userDeletionMapper;
+    private final UserPhoneMapper userPhoneMapper;
+    private final UserMailMapper userMailMapper;
+    private final RedissonClient redissonClient;
+    private final DistributedCache distributedCache;
+    private final AbstractChainContext<UserRegisterReqDTO> abstractChainContext;
+    private final RBloomFilter<String> userRegisterCachePenetrationBloomFilter;
+
+    @Override
+    public UserLoginRespDTO login(UserLoginReqDTO requestParam) {
+        String usernameOrMailOrPhone = requestParam.getUsernameOrMailOrPhone();
+        boolean mailFlag = false;
+        // 时间复杂度最佳 O(1)。indexOf or contains 时间复杂度为 O(n)
+        for (char c : usernameOrMailOrPhone.toCharArray()) {
+            if (c == '@') {
+                mailFlag = true;
+                break;
+            }
+        }
+        String username;
+        if (mailFlag) {
+            LambdaQueryWrapper<UserMailDO> queryWrapper = Wrappers.lambdaQuery(UserMailDO.class)
+                    .eq(UserMailDO::getMail, usernameOrMailOrPhone);
+            username = Optional.ofNullable(userMailMapper.selectOne(queryWrapper))
+                    .map(UserMailDO::getUsername)
+                    .orElseThrow(() -> new ClientException("用户名/手机号/邮箱不存在"));
+        } else {
+            LambdaQueryWrapper<UserPhoneDO> queryWrapper = Wrappers.lambdaQuery(UserPhoneDO.class)
+                    .eq(UserPhoneDO::getPhone, usernameOrMailOrPhone);
+            username = Optional.ofNullable(userPhoneMapper.selectOne(queryWrapper))
+                    .map(UserPhoneDO::getUsername)
+                    .orElse(null);
+        }
+        username = Optional.ofNullable(username).orElse(requestParam.getUsernameOrMailOrPhone());
+        LambdaQueryWrapper<UserDO> queryWrapper = Wrappers.lambdaQuery(UserDO.class)
+                .eq(UserDO::getUsername, username)
+                .eq(UserDO::getPassword, requestParam.getPassword())
+                .select(UserDO::getId, UserDO::getUsername, UserDO::getRealName);
+        UserDO userDO = userMapper.selectOne(queryWrapper);
+        if (userDO != null) {
+            UserInfoDTO userInfo = UserInfoDTO.builder()
+                    .userId(String.valueOf(userDO.getId()))
+                    .username(userDO.getUsername())
+                    .realName(userDO.getRealName())
+                    .build();
+            String accessToken = JWTUtil.generateAccessToken(userInfo);
+            UserLoginRespDTO actual = new UserLoginRespDTO(userInfo.getUserId(), requestParam.getUsernameOrMailOrPhone(), userDO.getRealName(), accessToken);
+            distributedCache.put(accessToken, JSON.toJSONString(actual), 30, TimeUnit.MINUTES);
+            return actual;
+        }
+        throw new ServiceException("账号不存在或密码错误");
+    }
+
+    @Override
+    public UserLoginRespDTO checkLogin(String accessToken) {
+        return distributedCache.get(accessToken, UserLoginRespDTO.class);
+    }
+
+    @Override
+    public void logout(String accessToken) {
+        if (StrUtil.isNotBlank(accessToken)) {
+            distributedCache.delete(accessToken);
+        }
+    }
+
+    @Override
+    public Boolean hasUsername(String username) {
+        boolean hasUsername = userRegisterCachePenetrationBloomFilter.contains(username);
+        if (hasUsername) {
+            StringRedisTemplate instance = (StringRedisTemplate) distributedCache.getInstance();
+            return instance.opsForSet().isMember(RedisKeyConstant.USER_REGISTER_REUSE_SHARDING + UserReuseUtil.hashShardingIdx(username), username);
+        }
+        return true;
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    @Override
+    public UserRegisterRespDTO register(UserRegisterReqDTO requestParam) {
+        abstractChainContext.handler(UserChainMarkEnum.USER_REGISTER_FILTER.name(), requestParam);
+        RLock lock = redissonClient.getLock(RedisKeyConstant.LOCK_USER_REGISTER + requestParam.getUsername());
+        boolean tryLock = lock.tryLock();
+        if (!tryLock) {
+            throw new ServiceException(UserRegisterErrorCodeEnum.HAS_USERNAME_NOTNULL);
+        }
+        try {
+            try {
+                int inserted = userMapper.insert(BeanUtil.convert(requestParam, UserDO.class));
+                if (inserted < 1) {
+                    throw new ServiceException(UserRegisterErrorCodeEnum.USER_REGISTER_FAIL);
+                }
+            } catch (DuplicateKeyException dke) {
+                log.error("用户名 [{}] 重复注册", requestParam.getUsername());
+                throw new ServiceException(UserRegisterErrorCodeEnum.HAS_USERNAME_NOTNULL);
+            }
+            UserPhoneDO userPhoneDO = UserPhoneDO.builder()
+                    .phone(requestParam.getPhone())
+                    .username(requestParam.getUsername())
+                    .build();
+            try {
+                userPhoneMapper.insert(userPhoneDO);
+            } catch (DuplicateKeyException dke) {
+                log.error("用户 [{}] 注册手机号 [{}] 重复", requestParam.getUsername(), requestParam.getPhone());
+                throw new ServiceException(UserRegisterErrorCodeEnum.PHONE_REGISTERED);
+            }
+            if (StrUtil.isNotBlank(requestParam.getMail())) {
+                UserMailDO userMailDO = UserMailDO.builder()
+                        .mail(requestParam.getMail())
+                        .username(requestParam.getUsername())
+                        .build();
+                try {
+                    userMailMapper.insert(userMailDO);
+                } catch (DuplicateKeyException dke) {
+                    log.error("用户 [{}] 注册邮箱 [{}] 重复", requestParam.getUsername(), requestParam.getMail());
+                    throw new ServiceException(UserRegisterErrorCodeEnum.MAIL_REGISTERED);
+                }
+            }
+            String username = requestParam.getUsername();
+            userReuseMapper.delete(Wrappers.update(new UserReuseDO(username)));
+            StringRedisTemplate instance = (StringRedisTemplate) distributedCache.getInstance();
+            instance.opsForSet().remove(RedisKeyConstant.USER_REGISTER_REUSE_SHARDING + UserReuseUtil.hashShardingIdx(username), username);
+            // 布隆过滤器设计问题：设置多大、碰撞率以及初始容量不够了怎么办？详情查看：https://t.zsxq.com/12DkAyTFS
+            userRegisterCachePenetrationBloomFilter.add(username);
+        } finally {
+            lock.unlock();
+        }
+        return BeanUtil.convert(requestParam, UserRegisterRespDTO.class);
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    @Override
+    public void deletion(UserDeletionReqDTO requestParam) {
+        String username = UserContext.getUsername();
+        if (!Objects.equals(username, requestParam.getUsername())) {
+            // 此处严谨来说，需要上报风控中心进行异常检测
+            throw new ClientException("注销账号与登录账号不一致");
+        }
+        RLock lock = redissonClient.getLock(RedisKeyConstant.USER_DELETION + requestParam.getUsername());
+        // 加锁为什么放在 try 语句外？https://www.yuque.com/magestack/12306/pu52u29i6eb1c5wh
+        lock.lock();
+        try {
+            UserQueryRespDTO userQueryRespDTO = userService.queryUserByUsername(username);
+            UserDeletionDO userDeletionDO = UserDeletionDO.builder()
+                    .idType(userQueryRespDTO.getIdType())
+                    .idCard(userQueryRespDTO.getIdCard())
+                    .build();
+            userDeletionMapper.insert(userDeletionDO);
+            UserDO userDO = new UserDO();
+            userDO.setDeletionTime(System.currentTimeMillis());
+            userDO.setUsername(username);
+            // MyBatis Plus 不支持修改语句变更 del_flag 字段
+            userMapper.deletionUser(userDO);
+            UserPhoneDO userPhoneDO = UserPhoneDO.builder()
+                    .phone(userQueryRespDTO.getPhone())
+                    .deletionTime(System.currentTimeMillis())
+                    .build();
+            userPhoneMapper.deletionUser(userPhoneDO);
+            if (StrUtil.isNotBlank(userQueryRespDTO.getMail())) {
+                UserMailDO userMailDO = UserMailDO.builder()
+                        .mail(userQueryRespDTO.getMail())
+                        .deletionTime(System.currentTimeMillis())
+                        .build();
+                userMailMapper.deletionUser(userMailDO);
+            }
+            distributedCache.delete(UserContext.getToken());
+            userReuseMapper.insert(new UserReuseDO(username));
+            StringRedisTemplate instance = (StringRedisTemplate) distributedCache.getInstance();
+            instance.opsForSet().add(RedisKeyConstant.USER_REGISTER_REUSE_SHARDING + UserReuseUtil.hashShardingIdx(username), username);
+        } finally {
+            lock.unlock();
+        }
+    }
+}
